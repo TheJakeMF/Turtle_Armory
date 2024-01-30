9523db737c86a3c20fb98b52dfde52b317991dd7
JakeMF <thejakemf@gmail.com>
2024-01-30 14:09:03 -0500

    Add project files.

---
+++ Turtle_Armory/data/gear.js
@@ -0,0 +1,56 @@
+﻿var gear = {
+    "head": [
+        {
+            "name": "Rune-Etched Crown",
+            "source": "Crafting",
+            "agi": 5,
+            "str": 25,
+            "hit": 1,
+            "ac": 534,
+            "phase": 7,
+            "id": 60291
+        },
+        {
+            "name": "Timeless Artificier's Optics",
+            "source": "Dungeon",
+            "ac": 157,
+            "sta": 17,
+            "ap": 30,
+            "hit": 1,
+            "phase": 7,
+            "id": 61039
+        },
+        {
+            "name": "Gurubashi Helm",
+            "str": 22,
+            "agi": 21,
+            "sta": 13,
+            "ac": 549,
+            "source": "ZG",
+            "phase": 4,
+            "id": 20263
+        }
+    ],
+    "neck": [
+        {
+            "name": "Feralkin Necklace",
+            "source": "Quest",
+            "skill": 4,
+            "type": "Fist",
+            "agi": 4,
+            "phase": 7,
+            "id": 61512
+        },
+        {
+            "name": "Touch of Frost",
+            "source": "Naxx",
+            "subsource": "frost",
+            "sta": 20,
+            "resist": {
+                "frost": 24
+            },
+            "phase": "6",
+            "id": 22935
+        }
+    ]
+};
