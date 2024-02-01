8d56556faac1cfae65ec07945a1ebae7ccb99f1b
JakeMF <thejakemf@gmail.com>
2024-01-31 23:22:12 -0500

    Setting up skeleton of site.

---
+++ Turtle_Armory/data/baseStats.js
@@ -0,0 +1,84 @@
+﻿var baseStats =
+{
+"warrior_dwarf": 
+    {
+        "str": 122,
+        "agi": 76,
+        "sta": 113,
+        "int": 29,
+        "spi": 44,
+        "health": 1689
+    },
+"warrior_gnome": 
+    {
+        "str": 115,
+        "agi": 83,
+        "sta": 109,
+        "int": 35,
+        "spi": 45,
+        "health": 1689
+    },
+"warrior_gnome": 
+    {
+        "str": 115,
+        "agi": 83,
+        "sta": 109,
+        "int": 35,
+        "spi": 45,
+        "health": 1689
+    },
+"warrior_human":
+    {
+        "str": 120,
+        "agi": 80,
+        "sta": 110,
+        "int": 30,
+        "spi": 47,
+        "health": 1689
+    },
+"warrior_nightelf": 
+    {
+        "str": 117,
+        "agi": 85,
+        "sta": 109,
+        "int": 30,
+        "spi": 45,
+        "health": 1689
+    },
+"warrior_orc": 
+    {
+        "str": 123,
+        "agi": 77,
+        "sta": 112,
+        "int": 27,
+        "spi": 48,
+        "health": 1689
+    },
+"warrior_tauren": 
+    {
+        "str": 125,
+        "agi": 75,
+        "sta": 112,
+        "int": 25,
+        "spi": 47,
+        "health": 1689
+    },
+"warrior_nightelf": 
+    {
+        "str": 117,
+        "agi": 85,
+        "sta": 109,
+        "int": 30,
+        "spi": 45,
+        "health": 1689
+    },
+"warrior_nightelf": 
+    {
+        "str": 117,
+        "agi": 85,
+        "sta": 109,
+        "int": 30,
+        "spi": 45,
+        "health": 1689
+    }
+};
